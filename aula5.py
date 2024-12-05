5fbf1e93d2c1ca17094fad68f95c1dfaf58cb483
Jessica Liberato <100487905+JehLiber@users.noreply.github.com>
2024-12-05 16:54:17 -0300

    Delete aula5.py

--- aula5.py
+++
@@ -1,8 +0,0 @@
-# tipo de dado bool(boolean)
-# valor booleano True ou False
-# só existem duas respostas possíveis:
-# sim(True) ou não(False).
-# Existem vários operadores para "questionar".
-# questiona se um valor é igual a outro
-print(10 == 10) # Sim => True (verdadeiro)
-print(10 == 20) # Não => False (falso)
